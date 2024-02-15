c6a79bdf62e8a65515333975ee8734561bbfa748
Rueda <alerueda2360@gmail.com>
2024-02-15 15:05:55 +0100

    cambio 2

--- models/clinica_diagnostico.py
+++ models/clinica_diagnostico.py
@@ -13,11 +13,11 @@ class ClinicaDiagnostico(models.Model):
 
     nombrePaciente = fields.Char(related='idPaciente.nombre', string='Nombre del Paciente')
     apellidosPaciente = fields.Char(related='idPaciente.apellidos', string='Nombre del Paciente')
-    nombreCompletoPaciente = nombrePaciente + " " + apellidosPaciente
+    
 
     nombreMedico = fields.Char(related='idMedico.nombre', string='Nombre del Paciente')
     apellidosMedico = fields.Char(related='idMedico.apellidos', string='Nombre del Paciente')
-    nombreCompletoMedico = nombreMedico + " " + apellidosMedico
+
     
     _sql_constraints = [
         ('diagnosito_unico', 'UNIQUE(paciente_id, medico_id, fecha_diagnostico)', 'Ya existe un diagnóstico para este paciente y médico en esta fecha.'),
