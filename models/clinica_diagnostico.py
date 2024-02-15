415bf2cc1132255a5eaf22078dbc4867d548f9c5
Rueda <alerueda2360@gmail.com>
2024-02-15 14:57:29 +0100

    toco la vista y diagnostico

--- models/clinica_diagnostico.py
+++ models/clinica_diagnostico.py
@@ -11,9 +11,14 @@ class ClinicaDiagnostico(models.Model):
     fecha_diagnostico = fields.Date('Fecha de diagnóstico', default=fields.Date.today(), required=True)
     diagnostico = fields.Text('Diagnóstico')
 
-    # Puedes agregar más campos según tus necesidades
+    nombrePaciente = fields.Char(related='idPaciente.nombre', string='Nombre del Paciente')
+    apellidosPaciente = fields.Char(related='idPaciente.apellidos', string='Nombre del Paciente')
+    nombreCompletoPaciente = nombrePaciente + " " + apellidosPaciente
 
-    # Restringir la creación de registros duplicados
+    nombreMedico = fields.Char(related='idMedico.nombre', string='Nombre del Paciente')
+    apellidosMedico = fields.Char(related='idMedico.apellidos', string='Nombre del Paciente')
+    nombreCompletoMedico = nombreMedico + " " + apellidosMedico
+    
     _sql_constraints = [
         ('diagnosito_unico', 'UNIQUE(paciente_id, medico_id, fecha_diagnostico)', 'Ya existe un diagnóstico para este paciente y médico en esta fecha.'),
     ]
