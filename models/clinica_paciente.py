85fbb707936ece4eee7648b05bf8de1014e8b6f1
Rueda <alerueda2360@gmail.com>
2024-02-15 13:31:51 +0100

    arreglos en seguridad

--- models/clinica_paciente.py
+++ models/clinica_paciente.py
@@ -4,9 +4,9 @@
 from odoo import models, fields
 
 class ClinicaMedico(models.Model):
-    _name = 'clinica.medico'
+    _name = 'clinica.paciente'
     _description = 'Médico en la clínica'
-    
+
     nombre = fields.Char('Nombre')
     apellidos = fields.Char('Apellido')
     sintomas = fields.Text('Síntomas')
